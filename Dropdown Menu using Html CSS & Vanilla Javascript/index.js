531df9e0cdf1d1779d7f35ae52e54e214e9792ef
charles16987 <106310641+charles16987@users.noreply.github.com>
2022-12-24 21:05:16 +0530

    Add files via upload

---
+++ Dropdown Menu using Html CSS & Vanilla Javascript/index.js
@@ -0,0 +1,9 @@
+
+let menuToggle = document.querySelector(".menuToggle")
+let navigation = document.querySelector('.navigation')
+
+
+
+menuToggle.addEventListener('click', ( ) => {
+    navigation.classList.toggle('active')
+})
